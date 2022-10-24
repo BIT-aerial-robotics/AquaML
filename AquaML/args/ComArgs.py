e6be6d0cb3d7f65b22a61011c71310ae4f227b93
yangtao121 <291843078@qq.com>
2022-10-24 14:03:29 +0800

    delet

--- AquaML/args/ComArgs.py
+++
@@ -1,6 +0,0 @@
-class NodeInfo:
-    def __init__(self, name: str):
-        """
-        This args describe single node. It is usually created automatically by node manager.
-        :param name: name of this node
-        """

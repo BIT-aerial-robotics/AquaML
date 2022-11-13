fccdcf628f1e3e13cf222bb8cf1009c1d87830be
yangtao <291843078@qq.com>
2022-11-13 16:26:31 +0800

    batch processing

--- AquaML/common/ImageTool.py
+++ AquaML/common/ImageTool.py
@@ -1,4 +1,6 @@
 import cv2
+from PIL import Image
+import numpy as np
 
 
 def save_from_array(imgs, path):
@@ -6,3 +8,14 @@ def save_from_array(imgs, path):
     for img in imgs:
         cv2.imwrite(path + '/{}.jpg'.format(str(i)), img[:, :, ::-1])
         i += 1
+
+def check_size(dir,shape, length, format='.png'):
+
+    for i in range(length):
+        name = dir + '/{}'.format(str(i)) + format
+        img = Image.open(name)
+        img = np.array(img)
+
+        if img.shape[0] != shape[0]:
+            img = cv2.resize(img,shape)
+            cv2.imwrite(name, img)

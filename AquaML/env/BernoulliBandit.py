2320ee90e16e484df683fc1d1d8260e79b46e83b
yangtao <291843078@qq.com>
2023-03-12 23:35:26 +0800

    update

--- AquaML/env/BernoulliBandit.py
+++ AquaML/env/BernoulliBandit.py
@@ -8,8 +8,10 @@ class BernoulliBandit(RLBaseEnv):
     def __init__(self, num_of_bandits, random_freq=0):
         super(BernoulliBandit, self).__init__()
         # 随机生成K个概率，作为K个老虎机的概率
-        self.probs = np.random.uniform(size=num_of_bandits)
-        self.best_idx = np.argmax(self.probs)  # 获奖概率最大的老虎机的索引
+        self.probs = np.random.uniform(size=num_of_bandits, low=0.1, high=0.3)
+
+        self.best_idx = np.random.randint(0, num_of_bandits - 1, 1)  # 获奖概率最大的老虎机的索引
+        self.probs[self.best_idx] = 0.95
         self.best_prob = self.probs[self.best_idx]  # 获奖概率最大的老虎机的概率
         self.K = num_of_bandits  # 老虎机的个数
 
@@ -18,27 +20,33 @@ class BernoulliBandit(RLBaseEnv):
 
         self.actions = np.zeros((num_of_bandits, 1))  # 存储运行时的动作
 
+        self.times = np.array([0, ]).reshape((1, 1))  # 记录运行的次数
+
         self.reset_count = 0
         self.random_freq = random_freq
 
         self._obs_info = DataInfo(
-            names=('reward2', 'last_action', 'best_idx', 'best_pr', 'pr'),
-            shapes=((1,), (1,), (1,), (1,), (1, )),
+            names=('reward2', 'last_action', 'times', 'best_idx', 'best_pr', 'pr'),
+            shapes=((1,), (1,), (1,), (1,), (1,), (1,)),
             dtypes=np.float32
         )
 
         self.count = 0
+        self.current_step_count = 0
 
     def random_bandit(self):
-        self.probs = np.random.uniform(size=self.K)
-        self.best_idx = np.argmax(self.probs)  # 获奖概率最大的老虎机的索引
-        self.best_prob = self.probs[self.best_idx]  # 获奖概率最大的老虎机的概率
+        self.probs = np.random.uniform(size=self.K, low=0.1, high=0.3)
+
+        self.best_idx = np.random.randint(0, self.K - 1, 1)  # 获奖概率最大的老虎机的索引
+        self.probs[self.best_idx] = 0.95
 
     def reset(self):
         self.reset_count += 1
         hidden_reset = False
-        if self.random_freq> 0:
+        if self.random_freq > 0:
+
             if self.reset_count % self.random_freq == 0:
+                self.times = 0
                 self.random_bandit()
                 self.last_action = np.array([0, ]).reshape((1, 1))  # 上一次的动作
                 self.last_reward = np.array([0, ]).reshape((1, 1))  # 上一次的奖励
@@ -46,6 +54,7 @@ class BernoulliBandit(RLBaseEnv):
 
         obs = {'reward2': self.last_reward,
                'last_action': self.last_action,
+               'times': np.array([self.times, ]).reshape((1, 1)),
                'best_idx': np.array([self.best_idx, ]).reshape((1, 1)),
                'best_pr': np.array([self.best_prob, ]).reshape((1, 1)),
                'pr': np.array(self.probs[self.last_action]).reshape((1, 1))
@@ -68,6 +77,7 @@ class BernoulliBandit(RLBaseEnv):
 
         obs = {'reward2': np.array([reward, ]).reshape((1, 1)),
                'last_action': deepcopy(self.last_action.reshape((1, 1))),
+               'times': np.array([self.times, ]).reshape((1, 1)),
                'best_idx': np.array([self.best_idx, ]).reshape((1, 1)),
                'best_pr': np.array([self.best_prob, ]).reshape((1, 1)),
                'pr': np.array(self.probs[action]).reshape((1, 1))
--- Tutorial/RL2_MultiBandit_PPO.py
+++ Tutorial/RL2_MultiBandit_PPO.py
@@ -18,30 +18,30 @@ Reference:
 2. Learning to Learn by Gradient Descent by Gradient Descent, 2016, https://arxiv.org/abs/1606.04474
 """
 
-num_of_bandits = 2
+num_of_bandits = 6
 
 
 class Actor_net(tf.keras.Model):
     def __init__(self):
         super(Actor_net, self).__init__()
 
-        self.lstm = tf.keras.layers.LSTM(32, input_shape=(2,), return_sequences=True, return_state=True)
+        self.lstm = tf.keras.layers.LSTM(64, input_shape=(3,), return_sequences=True, return_state=True)
 
         self.action_layer = tf.keras.layers.Dense(num_of_bandits, activation='softmax')
 
         self.learning_rate = 2e-4
 
-        self.output_info = {'action': (1,), 'hidden1': (32,), 'hidden2': (32,)}  # 离散变量较为特殊，输出维度为1
+        self.output_info = {'action': (1,), 'hidden1': (64,), 'hidden2': (64,)}  # 离散变量较为特殊，输出维度为1
 
-        self.input_name = ('reward2', 'last_action', 'hidden1', 'hidden2')
+        self.input_name = ('reward2', 'last_action', 'times', 'hidden1', 'hidden2')
 
         self.optimizer = 'Adam'
 
         self.rnn_flag = True
 
     @tf.function
-    def call(self, reward2, last_action, hidden1, hidden2):
-        x = tf.concat([reward2, last_action], axis=2)
+    def call(self, reward2, last_action, times, hidden1, hidden2):
+        x = tf.concat([reward2, last_action, times], axis=2)
         hidden_states = (hidden1, hidden2)
         whole_seq, last_seq, hidden_state = self.lstm(x, hidden_states)
         action = self.action_layer(whole_seq)
@@ -64,13 +64,13 @@ class Critic_net(tf.keras.Model):
 
         self.output_name = {'value': (1,)}
 
-        self.input_name = ('reward2', 'action', 'best_idx', 'best_pr', 'pr')
+        self.input_name = ('reward2', 'action', 'times', 'best_idx', 'best_pr', 'pr')
 
         self.optimizer = 'Adam'
 
     @tf.function
-    def call(self, reward, action, best_idx, best_pr, pr):
-        inputs = tf.concat([reward, action, best_idx, best_pr, pr], axis=1)
+    def call(self, reward, action, times, best_idx, best_pr, pr):
+        inputs = tf.concat([reward, action, times, best_idx, best_pr, pr], axis=1)
         x = self.dense1(inputs)
         value = self.value_layer(x)
         return value
@@ -79,21 +79,21 @@ class Critic_net(tf.keras.Model):
         pass
 
 
-env = BernoulliBandit(num_of_bandits=num_of_bandits, random_freq=12)
+env = BernoulliBandit(num_of_bandits=num_of_bandits, random_freq=5)
 
 ppo_parameter = PPO_parameter(
-    epoch_length=50,
+    epoch_length=100,
     n_epochs=2000,
-    total_steps=2400,
+    total_steps=2000,
     batch_size=256,
     update_times=1,
     update_actor_times=1,
     update_critic_times=1,
     gamma=0.,
-    epsilon=0.2,
-    lambada=0.95,
+    epsilon=0.01,
+    lambada=0.5,
     action_space_type='discrete',
-    eval_episodes=12,
+    eval_episodes=5,
     eval_interval=1,
     batch_trajectory=False
 )

更新KL bug
tfp库里面计算按照多维采样，需要还原

AquaML/rlalgo/AqauRL.py
@@ -6,7 +6,16 @@ from AquaML.core.ToolKit import SummaryRewardCollector, MDPCollector, mkdir
 from AquaML.core.RLToolKit import RLStandardDataSet
 import numpy as np
 from copy import deepcopy
+from dataclasses import dataclass
 import os
+
+
+@dataclass
+class LoadFlag:
+    actor: bool = False
+    critic: bool = False
+    state_normalizer: bool = False
+    reward_normalizer: bool = False
 
 
 class RunningMeanStd:
@@ -128,7 +137,7 @@ class Normalization:
             communicator.store_indicate_dict(
                 agent_name=agent_name,
                 indicate_dict=dic,
-                index= 0 if communicator.thread_manager.get_thread_id - 1 < 0 else communicator.thread_manager.get_thread_id - 1
+                index=0 if communicator.thread_manager.get_thread_id - 1 < 0 else communicator.thread_manager.get_thread_id - 1
             )
 
 
@@ -139,10 +148,14 @@ class RewardScaling:
         self.running_ms = RunningMeanStd(shape=self.shape, name='reward')
         self.R = np.zeros(self.shape)
 
-    def update(self, x):
-        self.R = self.gamma * self.R + x
-        self.running_ms.update_(self.R)
-        x = x / (self.running_ms.std + 1e-8)  # Only divided std
+    def update(self, x, update=True):
+        if update:
+            self.R = self.gamma * self.R + x
+            self.running_ms.update_(self.R)
+            x = x / (self.running_ms.std + 1e-8)  # Only divided std
+        else:
+            x = (x - self.running_ms.mean) / (self.running_ms.std + 1e-8)
+
         return x
 
     def __call__(self, x):
@@ -188,6 +201,8 @@ class AquaRL(BaseAqua):
                  state_norm: bool = False,
                  reward_norm: bool = False,
                  snyc_norm_per: int = 1,
+                 check_point_path: str = None,
+                 load_flag: LoadFlag = LoadFlag(),
                  eval_env=None,
                  comm=None,
                  ):
@@ -441,26 +456,63 @@ class AquaRL(BaseAqua):
 
         # 状态处理插件，暂时仅支持vectorized env
 
-        self.obs_normalizer = Normalization(
-            obs_shape_dict=self.env.obs_info.shape_dict,
-        )
-
-        self.reward_normalizer = RewardScaling(1, self.agent_params.gamma)
-
         self._tool_dict['scaler'] = []
 
         if self.env_type == 'Vec':
             if state_norm:
-                self.worker.add_obs_plugin(self.obs_normalizer)
+
+                self.obs_normalizer = Normalization(
+                    obs_shape_dict=self.env.obs_info.shape_dict,
+                )
+
+                self.state_norm_flag = True
+                self.worker.add_obs_plugin((self.obs_normalizer, self.state_norm_flag))
                 self._tool_dict['scaler'].append(self.obs_normalizer)
+            else:
+                self.state_norm_flag = False
+                self.obs_normalizer = None
 
             if reward_norm:
-                self.worker.add_reward_plugin(self.reward_normalizer)
+                self.reward_normalizer = RewardScaling(1, self.agent_params.gamma)
                 self._tool_dict['scaler'].append(self.reward_normalizer)
+                self.reward_norm_flag = True
+                self.worker.add_reward_plugin((self.reward_normalizer, self.reward_norm_flag))
+            else:
+                self.reward_normalizer = None
+                self.reward_norm_flag = False
             # self.worker.add_obs_plugin(self.obs_normalizer)
 
         self.snyc_norm_per = snyc_norm_per
 
+        if check_point_path is not None:
+            # check the path contains scaler folder if exists, create normalizer and load scaler
+            if load_flag.state_normalizer:
+                path = os.path.join(check_point_path, 'scaler')
+                if self.state_norm_flag is False:
+                    self.obs_normalizer = Normalization(
+                        obs_shape_dict=self.env.obs_info.shape_dict,
+                    )
+
+                    self.worker.add_obs_plugin((self.obs_normalizer, self.state_norm_flag))
+
+                self.obs_normalizer.load(path)
+
+            if load_flag.reward_normalizer:
+                path = os.path.join(check_point_path, 'scaler')
+                if self.reward_norm_flag is False:
+                    self.reward_normalizer = RewardScaling(1, self.agent_params.gamma)
+                    self.worker.add_reward_plugin((self.reward_normalizer, self.reward_norm_flag))
+
+                self.reward_normalizer.load(path)
+
+            if load_flag.actor:
+                path = os.path.join(check_point_path, 'actor.h5')
+                self.agent.actor.load_weights(path)
+
+            if load_flag.critic:
+                if self.optimize_enable:
+                    path = os.path.join(check_point_path, 'critic.h5')
+                    self.agent.critic.load_weights(path)
 
     def sampling(self):
 
@@ -540,20 +592,20 @@ class AquaRL(BaseAqua):
                         indicate_data = self.communicator.get_indicate_pool_dict(self.agent.name)
                         obs_norm_dict = {}
 
-                        for name in self.obs_names:
-                            mean = indicate_data[name + '_mean'][0]
-                            std = indicate_data[name + '_std'][0]
+                        if self.state_norm_flag:
+                            for name in self.obs_names:
+                                mean = indicate_data[name + '_mean'][0]
+                                std = indicate_data[name + '_std'][0]
 
-                            obs_norm_dict[name] = (mean, std)
+                                obs_norm_dict[name] = (mean, std)
 
-                            self.obs_normalizer.set_data(obs_norm_dict)
+                                self.obs_normalizer.set_data(obs_norm_dict)
 
-                        # total_reward = np.mean(indicate_data['total_reward'])
+                        if self.reward_norm_flag:
+                            total_reward_mean = indicate_data['total_reward_mean'][0]
+                            total_reward_std = indicate_data['total_reward_std'][0]
 
-                        total_reward_mean = indicate_data['total_reward_mean'][0]
-                        total_reward_std = indicate_data['total_reward_std'][0]
-
-                        self.reward_normalizer.set_data((total_reward_mean, total_reward_std))
+                            self.reward_normalizer.set_data((total_reward_mean, total_reward_std))
 
             self.communicator.thread_manager.Barrier()
 
@@ -563,19 +615,18 @@ class AquaRL(BaseAqua):
 
                 # 获取子线程normalize的数据
                 if self.env_type == 'Vec':
-                    if self.obs_normalizer is not None:
+                    if self.state_norm_flag:
                         self.obs_normalizer.push_to_communicator(
                             communicator=self.communicator,
                             agent_name=self.agent.name,
                         )
-                    if self.reward_normalizer is not None:
+                    if self.reward_norm_flag:
                         self.reward_normalizer.push_to_communicator(
                             communicator=self.communicator,
                             agent_name=self.agent.name,
                         )
 
             self.communicator.thread_manager.Barrier()
-
 
             if self.optimize_enable:
                 print('####################{}####################'.format(epoch + 1))
@@ -605,45 +656,56 @@ class AquaRL(BaseAqua):
                 self.recoder.record_scalar(loss_info, epoch + 1)
 
                 if self.env_type == 'Vec':
+
                     indicate_data = self.communicator.get_indicate_pool_dict(self.agent.name)
                     obs_norm_dict = {}
 
-                    for name in self.obs_names:
-                        mean = np.mean(indicate_data[name + '_mean'], axis=0)
+                    if self.state_norm_flag:
+                        for name in self.obs_names:
+                            mean = np.mean(indicate_data[name + '_mean'], axis=0)
 
-                        std_2 = np.sum(indicate_data[name + '_std'] ** 2, axis=0)
-                        std = std_2/self.worker_thread
-                        std = np.sqrt(std)
+                            std_2 = np.sum(indicate_data[name + '_std'] ** 2, axis=0)
+                            std = std_2 / self.worker_thread
+                            std = np.sqrt(std)
 
-                        obs_norm_dict[name] = (mean, std)
+                            obs_norm_dict[name] = (mean, std)
 
-                        self.communicator.store_indicate_dict(
-                            agent_name=self.agent.name,
-                            indicate_dict={name + '_mean': mean, name + '_std': std},
-                            index=0,
-                        )
+                            self.communicator.store_indicate_dict(
+                                agent_name=self.agent.name,
+                                indicate_dict={name + '_mean': mean, name + '_std': std},
+                                index=0,
+                            )
 
-                        self.obs_normalizer.set_data(obs_norm_dict)
+                            self.obs_normalizer.set_data(obs_norm_dict)
 
                     # total_reward = np.mean(indicate_data['total_reward'])
 
-                    total_reward_mean = np.mean(indicate_data['total_reward_mean'])
-                    total_reward_std = np.mean(indicate_data['total_reward_std'])
+                    if self.reward_norm_flag:
+                        total_reward_mean = np.mean(indicate_data['total_reward_mean'])
+                        total_reward_std = np.mean(indicate_data['total_reward_std'])
 
-                    self.reward_normalizer.set_data((total_reward_mean, total_reward_std))
+                        self.reward_normalizer.set_data((total_reward_mean, total_reward_std))
 
-                    self.communicator.store_indicate_dict(
-                        agent_name=self.agent.name,
-                        indicate_dict={'total_reward_mean': total_reward_mean, 'total_reward_std': total_reward_std},
-                        index=0,
-                    )
+                        self.communicator.store_indicate_dict(
+                            agent_name=self.agent.name,
+                            indicate_dict={'total_reward_mean': total_reward_mean,
+                                           'total_reward_std': total_reward_std},
+                            index=0,
+                        )
+
+                    for key, value in self._tool_dict.items():
+                        cache_path = os.path.join(self.file_system.get_cache_path(self.agent.name), key)
+                        # th_id = self.communicator.thread_manager.get_thread_id
+                        # cache_path = os.path.join(cache_path, str(th_id))
+                        for tool in value:
+                            tool.save(cache_path)
 
                 if epoch % self.agent_params.checkpoint_interval == 0:
                     self.recoder.save_checkpoint(
                         model_dict=self.agent.get_all_model_dict,
                         epoch=epoch + 1,
                         checkpoint_dir=self.file_system.get_history_model_path(self.agent.name),
-                        # tool=self._tool_dict,
+                        tool=self._tool_dict,
                     )
 
                 # sync normalizer

AquaML/rlalgo/ExplorePolicy.py
@@ -29,7 +29,7 @@ def create_explor_policy(explore_policy_name, shape, actor_out_names):
             infos.append(info)
 
     return policy, infos
-            
+
 
 class OrnsteinUhlenbeckActionNoise:
     def __init__(self, mu, sigma, theta=.15, dt=1e-2, x0=None):
@@ -49,7 +49,6 @@ class OrnsteinUhlenbeckActionNoise:
 
     def reset(self):
         self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
-
 
 
 # TODO: 探索策略的创建需要优化
@@ -86,6 +85,7 @@ class ExplorePolicyBase(abc.ABC):
         """
         Scale the action to the range of environment.
         """
+
     @abc.abstractmethod
     def test_action(self, *args, **kwargs):
         """
@@ -134,7 +134,6 @@ class ExplorePolicyBase(abc.ABC):
         """
         pass
 
-
     def create_info(self):
         """
         创建探索策略需要的额外输入，比如高斯策略，sigma的输入
@@ -142,7 +141,7 @@ class ExplorePolicyBase(abc.ABC):
         dic = {}
 
         return dic
-    
+
     @property
     def get_aditional_output(self):
         return self._aditional_output
@@ -190,8 +189,8 @@ class GaussianExplorePolicy(ExplorePolicyBase):
 
     def resample_prob(self, mu, std, action):
         # sigma = tf.exp(log_std)
-        dist = tfp.distributions.Normal(loc=mu, scale=std)
-        log_prob = dist.log_prob(action)
+        noise = (action - mu) / std
+        log_prob = self.dist.log_prob(noise)
         return log_prob
 
     def get_entropy(self, mean, log_std):
@@ -201,7 +200,7 @@ class GaussianExplorePolicy(ExplorePolicyBase):
 
     def test_action(self, mu, log_std):
         return mu, tf.ones((1, *self.shape))
-    
+
     def create_info(self):
         log_std = {
             'name': 'log_std',
@@ -262,29 +261,26 @@ class CategoricalExplorePolicy(ExplorePolicyBase):
         return sample_action, prob
 
     def resample_prob(self, log_prob, action):
-
         dist = tfp.distributions.Categorical(logits=log_prob)
         log_prob = dist.log_prob(action)
         return log_prob
 
     def test_action(self, action):
-
         action = tf.argmax(action, axis=-1)
 
         return action, tf.ones((1, *self.shape))
 
 
 class OrnsteinUhlenbeckExplorePolicy(ExplorePolicyBase):
-
     """
     该探索策略用于确定性策略。
     """
+
     def __init__(self, shape, mu=0, sigma=0.2, theta=0.15, dt=1e-2, x0=None):
         super().__init__(shape)
         self.input_name = ('action',)
         self.noise = OrnsteinUhlenbeckActionNoise(mu, sigma, theta, dt, x0)
 
-    
     def noise_and_prob(self, batch_size=1):
         pass
 
@@ -292,13 +288,12 @@ class OrnsteinUhlenbeckExplorePolicy(ExplorePolicyBase):
         noise = self.noise()
         action = action + noise
         return action, tf.ones((1, *self.shape))
-        
 
     def resample_prob(self, log_prob, action):
         pass
 
     def test_action(self, action):
         return action, tf.ones((1, *self.shape))
-    
+
     def reset(self):
         self.noise.reset()

AquaML/rlalgo/PPOAgent.py
@@ -6,6 +6,8 @@ from AquaML.rlalgo.AgentParameters import PPOAgentParameter
 from AquaML.core.Comunicator import Communicator
 from AquaML.core.RLToolKit import RLStandardDataSet
 from AquaML.buffer.RLPrePlugin import ValueFunctionComputer, GAEComputer, SplitTrajectory
+import copy
+import tensorflow_probability as tfp
 
 
 class PPOAgent(BaseRLAgent):
@@ -162,15 +164,16 @@ class PPOAgent(BaseRLAgent):
     def train_actor(self,
                     actor_inputs: tuple,
                     advantage: tf.Tensor,
-                    old_log_prob: tf.Tensor,
+                    old_prob: tf.Tensor,
                     action: tf.Tensor,
                     clip_ratio: float,
                     entropy_coef: float,
                     ):
-        old_log_prob = tf.math.log(old_log_prob)
+        # cancel state std
         with tf.GradientTape() as tape:
             tape.watch(self.actor_train_vars)
 
+            old_log_prob = tf.math.log(old_prob)
             out = self.resample_prob(actor_inputs, action)
 
             log_prob = out[0]
@@ -263,6 +266,12 @@ class PPOAgent(BaseRLAgent):
 
         early_stop = False
 
+        log_std = getattr(self, 'tf_log_std', None)
+        if log_std is not None:
+            old_log_std = copy.deepcopy(log_std)
+        else:
+            old_log_std = None
+
         for i in range(self.agent_params.update_times):
             for batch_data in train_data(self.agent_params.batch_size):
                 actor_input_obs = []
@@ -304,31 +313,43 @@ class PPOAgent(BaseRLAgent):
                         actor_optimize_info, log_prob = self.train_actor(
                             actor_inputs=actor_input_obs,
                             advantage=advantage,
-                            old_log_prob=batch_data['prob'],
+                            old_prob=batch_data['prob'],
                             action=batch_data['action'],
                             clip_ratio=self.agent_params.clip_ratio,
                             entropy_coef=self.agent_params.entropy_coef,
                         )
                         self.loss_tracker.add_data(actor_optimize_info, prefix='actor')
 
-                    # compute kl divergence
-                    # old_log_prob = tf.math.log(batch_data['prob'])
-                    # log_ratio = log_prob - old_log_prob
-                    # approx_kl_div = tf.reduce_mean((tf.exp(log_ratio) - 1) - log_ratio).numpy()
-
-                    # compute kl divergence
+                    # compute kl divergence, general type
                     old_log_prob = tf.math.log(batch_data['prob'])
                     log_ratio = log_prob - old_log_prob
                     approx_kl_div = tf.reduce_mean((tf.exp(log_ratio) - 1) - log_ratio).numpy()
+
+                    # compute kl divergence
+                    # if old_log_std is not None:
+                    #     old_log_prob = tf.math.log(batch_data['prob'])
+                    #     log_ratio = log_prob - old_log_prob
+                    #     approx_kl_div = tf.reduce_mean((tf.exp(log_ratio) - 1) - log_ratio).numpy()
+                    # else:
+                    #     new_log_std = getattr(self, 'tf_log_std')
+                    # 
+                    #     old_dist = tfp.distributions.Normal(batch_data['mu'], tf.exp(old_log_std))
+                    #     new_dist = tfp.distributions.Normal(batch_data['mu'], tf.exp(new_log_std))
+                    #     approx_kl_div = tf.reduce_mean(tfp.distributions.kl_divergence(old_dist, new_dist)).numpy()
 
                     self.loss_tracker.add_data({'approx_kl_div': approx_kl_div}, prefix='kl_div')
 
                     # KL_div = tf.reduce_sum
 
-                    if approx_kl_div > self.agent_params.target_kl*1.5:
-                        print('Early stopping at step {} due to reaching max kl.' .format(i))
-                        early_stop = True
-                        break
+                    if self.agent_params.target_kl is not None:
+                        if approx_kl_div > self.agent_params.target_kl * 1.5:
+                            print('Early stopping at step {} due to reaching max kl.'.format(i))
+                            early_stop = True
+                            break
+                    # if approx_kl_div > self.agent_params.target_kl*1.5:
+                    #     print('Early stopping at step {} due to reaching max kl.' .format(i))
+                    #     early_stop = True
+                    #     break
 
                 if early_stop:
                     break

Tutorial2/BugDetect.py
@@ -6,7 +6,7 @@ from AquaML.Tool import allocate_gpu
 #
 # comm = MPI.COMM_WORLD
 # allocate_gpu(comm, 0)
-from AquaML.rlalgo.AqauRL import AquaRL
+from AquaML.rlalgo.AqauRL import AquaRL, LoadFlag
 from AquaML.rlalgo.AgentParameters import PPOAgentParameter
 from AquaML.rlalgo.PPOAgent import PPOAgent
 import numpy as np
@@ -62,11 +62,11 @@ class Actor_net(tf.keras.Model):
         self.dense1 = tf.keras.layers.Dense(64, activation='relu')
         self.dense2 = tf.keras.layers.Dense(64, activation='relu')
         self.action_layer = tf.keras.layers.Dense(1, activation='tanh')
-        self.log_std = tf.keras.layers.Dense(1)
+        # self.log_std = tf.keras.layers.Dense(1)
 
         # self.learning_rate = 2e-5
 
-        self.output_info = {'action': (1,), 'log_std': (1,)}
+        self.output_info = {'action': (1,),}
 
         self.input_name = ('obs',)
 
@@ -83,9 +83,9 @@ class Actor_net(tf.keras.Model):
         x = self.dense1(obs)
         x = self.dense2(x)
         action = self.action_layer(x)
-        log_std = self.log_std(x)
+        # log_std = self.log_std(x)
 
-        return (action,log_std,)
+        return (action,)
 
     def reset(self):
         pass
@@ -207,9 +207,9 @@ parameters = PPOAgentParameter(
     eval_episode_length=200,
     entropy_coef=0.01,
     batch_advantage_normalization=True,
-    checkpoint_interval=2,
+    checkpoint_interval=20,
     min_steps=200,
-    target_kl=1,
+    target_kl=0.01,
 )
 
 agent_info_dict = {
@@ -217,6 +217,14 @@ agent_info_dict = {
     'critic': Critic_net,
     'agent_params': parameters,
 }
+
+
+load_flag = LoadFlag(
+    actor=True,
+    critic=False,
+    state_normalizer=True,
+    reward_normalizer=False
+)
 
 rl = AquaRL(
     env=vec_env,
@@ -226,8 +234,10 @@ rl = AquaRL(
     # comm=comm,
     name='debug',
     reward_norm=True,
-    state_norm=True,
+    state_norm=False,
     decay_lr=True,
+    # check_point_path='cache',
+    # load_flag=load_flag,
 )
 
 rl.run()
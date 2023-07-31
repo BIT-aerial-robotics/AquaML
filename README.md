f6dc9d8c4935ee0d82cbf5a769d8aff065f28e0a
yangtao <291843078@qq.com>
2023-07-31 20:00:39 +0800

    更新教程

--- README.md
+++ README.md
@@ -16,3 +16,85 @@ pip install keras-core
 
 ### Create a self defined model
 
+You do not need to concentrate on how the model will be used in the framework. You just need to define a model class which inherits from `Model` and implement the `call` method. Some algorithms may need to implement special functions. 
+
+
+Now we give an example of a simple model. The model is used in reinforcement learning proximal policy gradient. The input of the model is image with shape (64,64,1) and a tensor with shape (6,). And we will use CNN to extract features from the image and concatenate the features with the tensor. Then we will use a LSTM layer to extract the temporal features. Finally, we will use a dense layer to output the action.
+
+The input of `call` must contain `mask=None` even though you do not use it. 
+
+```python
+import tensorflow as tf
+
+class Actor_net(tf.keras.Model):
+    def __init__(self, ):
+        super(Actor_net, self).__init__()
+
+        self.encoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=8,
+                    kernel_size=7,
+                    input_shape=(64, 64, 1),
+                    activation='relu',
+                ),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2, name='pool1'),
+                tf.keras.layers.Conv2D(filters=16, kernel_size=4, activation='relu', name="conv2", ),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2, name="pool2"),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, name="IMG_layer1"),
+                tf.keras.layers.LeakyReLU(0.15),
+                tf.keras.layers.Dense(64, name="IMG_layer2"),
+                tf.keras.layers.LeakyReLU(0.15),
+            ])
+
+        self.lstm = tf.keras.layers.LSTM(256, return_sequences=True, return_state=True, name="LSTM_layer")
+
+        self.dense = tf.keras.layers.Dense(128, name="dense_layer")
+
+        self.leaky_relu1 = tf.keras.layers.LeakyReLU(0.2)
+
+        self.dense2 = tf.keras.layers.Dense(64, name="dense_layer2")
+
+        self.leaky_relu2 = tf.keras.layers.LeakyReLU(0.2)
+
+        self.action_layer = tf.keras.layers.Dense(4, name="action_layer")
+
+        # config input and output
+        self.output_info = {'action': (4,), 'hidden1': (256,), 'hidden2': (256,)}
+        self.input_name = ('img', 'actor_obs', 'hidden1', 'hidden2')
+
+        # config optimizer
+        self.optimizer_info = {
+            'type': 'Adam',
+            'args': {'learning_rate': 3e-4,
+                     'epsilon': 1e-5,
+                     'clipnorm': 0.5,
+                     },
+        }
+
+        # config special flag, it is used for special network, such as rnn
+        self.rnn_flag = True
+
+    def call(self, img, actor_obs, hidden1, hidden2, mask=None):
+        encode = self.encoder(img)
+
+        # When use rnn, if encode can't output as (batch_size, time_step, 64), use this reshape
+        encode = tf.reshape(encode, (actor_obs.shape[0], actor_obs.shape[1], 64))
+
+        x = tf.concat([encode, actor_obs], axis=-1)
+
+        x, hidden1, hidden2 = self.lstm(x, initial_state=[hidden1, hidden2], mask=mask)
+
+        x = self.dense(x)
+        x = self.leaky_relu1(x)
+        x = self.dense2(x)
+        x = self.leaky_relu2(x)
+        action = self.action_layer(x)
+
+        return action, hidden1, hidden2
+```
+
+In all algorithms, you need to specify the output and input of the model. The output and input are specified by `output_info` and `input_name`. The `output_info` is a dictionary, the key is the name of the output, and the value is the shape of the output. The `input_name` is a tuple, the elements of the tuple are the name of the input. The order of the elements in the tuple is the same as the order of the input in the `call` method.
+
+
--- test.py
+++ test.py
@@ -2,78 +2,113 @@ import tensorflow as tf
 import numpy as np
 
 
-class Actor_net(tf.keras.Model):
-
+# CNN+LSTM 测试 多模态输入
+class CNNLSTM(tf.keras.Model):
     def __init__(self):
-        super(Actor_net, self).__init__()
+        super(CNNLSTM, self).__init__()
 
-        self.lstm = tf.keras.layers.LSTM(32, input_shape=(2,), return_sequences=True, return_state=True)
-        # if using batch time trajectory, return state must be True
-        self.dense2 = tf.keras.layers.Dense(64)
+        # 创建encoder
 
-        self.action_dense = tf.keras.layers.Dense(64)
-        self.action_dense2 = tf.keras.layers.Dense(64)
-        self.action_layer = tf.keras.layers.Dense(1, activation='tanh')
+        self.encoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=8,
+                    kernel_size=7,
+                    input_shape=(64, 64, 1),
+                    activation='relu',
+                ),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2, name='pool1'),
+                tf.keras.layers.Conv2D(filters=16, kernel_size=4, activation='relu', name="conv2", ),
+                tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2, name="pool2"),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, name="IMG_layer1"),
+                tf.keras.layers.LeakyReLU(0.2),
+                tf.keras.layers.Dense(64, name="IMG_layer2"),
+                tf.keras.layers.LeakyReLU(0.2),
+            ]
+        )
 
-        self.value_dense = tf.keras.layers.Dense(64)
-        self.value_dense2 = tf.keras.layers.Dense(64)
-        self.value_layer = tf.keras.layers.Dense(1)
+        # self.reshape_layer = tf.keras.layers.Reshape((1, 64))
 
-        self.leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.2)
+        self.lstm = tf.keras.layers.LSTM(256, return_sequences=True, return_state=True, name="LSTM_layer")
 
-        # self.learning_rate = self.learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
-        #     initial_learning_rate=0.01,
-        #     decay_steps=1,
-        #     decay_rate=0.9,
-        #
-        # )
+        self.dense = tf.keras.layers.Dense(128, name="dense_layer")
 
-        self.learning_rate = 2e-3
+        self.dense2 = tf.keras.layers.Dense(64, name="dense_layer2")
 
-        self.rnn_flag = True
+        self.action_layer = tf.keras.layers.Dense(4, name="action_layer")
+    @tf.function
+    def call(self, img, obs, hidden1, hidden2, mask=None):
+        img = self.encoder(img)
+        # img = self.reshape_layer(img)
+        # img = tf.expand_dims(img, axis=1)
 
-        self.output_info = {'action': (1,), 'fusion_value': (1,), 'hidden1': (32,), 'hidden2': (32,)}
+        img = tf.reshape(img, (obs.shape[0], obs.shape[1], 64))
 
-        self.input_name = ('pos', 'hidden1', 'hidden2')
+        x = tf.concat([img, obs], axis=-1)
 
-        self.optimizer = 'Adam'
+        x, hidden1, hidden2 = self.lstm(x, initial_state=[hidden1, hidden2], mask=mask)
 
-    # @tf.function
-    def call(self, vel, hidden1, hidden2, mask=None):
-        hidden_states = (hidden1, hidden2)
-        whole_seq, last_seq, hidden_state = self.lstm(vel, hidden_states, mask=mask)
-        x = self.dense2(whole_seq)
-        x = self.leaky_relu(x)
-        action_x = self.action_dense(x)
-        action_x = self.leaky_relu(action_x)
-        action_x = self.action_dense2(action_x)
-        action_x = self.leaky_relu(action_x)
-        action = self.action_layer(action_x)
+        x = self.dense(x)
+        x = self.dense2(x)
+        action = self.action_layer(x)
 
-        value_x = self.value_dense(x)
-        value_x = self.leaky_relu(value_x)
-        value_x = self.value_dense2(value_x)
-        value_x = self.leaky_relu(value_x)
-        value = self.value_layer(value_x)
-
-        return (action, value, last_seq, hidden_state)
-
-    def reset(self):
-        pass
+        return action, hidden1, hidden2
 
 
+pad_sequences = tf.keras.utils.pad_sequences
 
+img = tf.random.normal((16, 64, 64, 1))
 
-model = Actor_net()
+obs = tf.random.normal((2, 8, 6))
 
-a = tf.ones((1, 20, 2), dtype=tf.float32)
+hidden1 = tf.random.normal((2, 256))
+hidden2 = tf.random.normal((2, 256))
 
-hidden1 = tf.ones((1, 32), dtype=tf.float32)
-hidden2 = tf.ones((1, 32), dtype=tf.float32)
-masks = np.zeros((1, 20))
-tf_masks = tf.convert_to_tensor(masks, dtype=tf.bool)
+model = CNNLSTM()
 
-model(a, hidden1, hidden2)
+action, hidden1, hidden2 = model(img, obs, hidden1, hidden2)
 
+target = tf.random.normal((2, 8, 4))
 
-model(a, hidden1, hidden2, mask=tf_masks)
+seq_obs1 = tf.random.normal((8, 6))
+seq_obs2 = tf.random.normal((6, 6))
+seq_obs3 = tf.random.normal((4, 6))
+
+seq_obs = [seq_obs1, seq_obs2, seq_obs3]
+
+pad_seq_obs = pad_sequences(seq_obs, padding='post', dtype='float32')
+
+seq_img1 = tf.random.normal((8, 64, 64, 1))
+seq_img2 = tf.random.normal((6, 64, 64, 1))
+seq_img3 = tf.random.normal((4, 64, 64, 1))
+
+seq_img = [seq_img1, seq_img2, seq_img3]
+
+pad_seq_img_ = pad_sequences(seq_img, padding='post', dtype='float32')
+pad_seq_img = tf.reshape(pad_seq_img_, (-1, 64, 64, 1))
+
+seq_bool_mask1 = tf.ones((8, 1), dtype=tf.bool)
+seq_bool_mask2 = tf.ones((6, 1), dtype=tf.bool)
+seq_bool_mask3 = tf.ones((4, 1), dtype=tf.bool)
+
+seq_bool_mask = [seq_bool_mask1, seq_bool_mask2, seq_bool_mask3]
+
+pad_seq_bool_mask = pad_sequences(seq_bool_mask, padding='post', dtype='bool')
+pad_seq_bool_mask = tf.squeeze(pad_seq_bool_mask, axis=-1)
+
+seq_hidden1 = tf.random.normal((3, 256))
+seq_hidden2 = tf.random.normal((3, 256))
+
+target = tf.random.normal((3, 8, 4))
+
+with tf.GradientTape() as tape:
+    action, hidden1, hidden2 = model(pad_seq_img, pad_seq_obs, seq_hidden1, seq_hidden2, mask=pad_seq_bool_mask)
+
+    value = tf.square(action - target)
+    mask_value = tf.boolean_mask(value, pad_seq_bool_mask)
+    loss = tf.reduce_mean(tf.square(mask_value))
+
+grads = tape.gradient(loss, model.trainable_variables)
+
+print(grads)
